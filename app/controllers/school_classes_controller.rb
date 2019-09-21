81cefa39629bf3e6363c542b7ee34428ce168727
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-21 00:45:39 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,28 @@
+class StudentsController < ApplicationController
+  def show
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+
+    redirect_to student_path(@student)
+  end
+
+  def post_params(*args)
+    params.require(:student).permit(*args)
+  end
+end
